89ca13da2393a33190115ebc50855c3418ae3214
Mladen Makivic <makivic.mladen1203@gmail.com>
2022-11-20 20:45:47 -0500

    added some logic to the board, ability to choose X or O without repeating

--- src/App.js
+++ src/App.js
@@ -4,18 +4,40 @@ import Square from './Components/Square';
 
 function App() {
   const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""]);
+  const [player, setPlayer] = useState('X');
+
+  const chooseSquare = (square) => {
+    setBoard(board.map((val, idx) => {
+      if (idx === square && val === "") {
+        return player;
+      }
+      return val;
+    })
+    );
+    if(player === "X"){
+      setPlayer("0");
+    } else {
+      setPlayer("X");
+    }
+  };
   return (
     <div className="App">
       <div className='board'>
         <div className='row'>
-          <Square
-            val={board[0]}
-            chooseSquare={() => {
-              alert(0);
-            }} />
+          <Square val={board[0]} chooseSquare={() => { chooseSquare(0) }} />
+          <Square val={board[1]} chooseSquare={() => { chooseSquare(1) }} />
+          <Square val={board[2]} chooseSquare={() => { chooseSquare(2) }} />
         </div>
-        <div className='row'></div>
-        <div className='row'></div>
+        <div className='row'>
+          <Square val={board[0]} chooseSquare={() => { chooseSquare(3) }} />
+          <Square val={board[1]} chooseSquare={() => { chooseSquare(4) }} />
+          <Square val={board[2]} chooseSquare={() => { chooseSquare(5) }} />
+        </div>
+        <div className='row'>
+          <Square val={board[0]} chooseSquare={() => { chooseSquare(6) }} />
+          <Square val={board[1]} chooseSquare={() => { chooseSquare(7) }} />
+          <Square val={board[2]} chooseSquare={() => { chooseSquare(8) }} />
+        </div>
       </div>
     </div>
   );
